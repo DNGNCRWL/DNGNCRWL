8a54ca82dd8530efb7183ee2dbdf88b2019e9bc7
Connor-yol <Jhofmann@uwm.edu>
2022-02-20 15:08:00 -0600

    Spider Model / Enemy Movement
    Spider prefab included. Enemy movement included in enemy_AI.cs. Navigation.cs changed to include a boolean value to check if player moved. BASIC implementation of the movement system. BEWARE, some files may have content that is unnecessary, it won't harm the environment but just know this commit is in no means perfect.

---
+++ Assets/Models/Test/enemy_AI.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.AI;
+using static Navigation;
+
+public class enemy_AI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    public Vector3 currentLoc;
+
+    public float xmove;
+    public float zmove;
+
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float sightRange;
+
+    public bool isInRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("remodel_tarantula(export)").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        isInRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        xmove = currentLoc.x - transform.position.x;
+        zmove = currentLoc.z - transform.position.z;
+
+        if(zmove + xmove >= 4 || zmove + xmove <=-4)
+        {
+            currentLoc = transform.position;
+            zmove = 0;
+            xmove = 0;
+            Patroling();
+        }
+
+        if (!isInRange) Patroling();
+        else
+        if(Navigation.isMoving)
+        {
+            chasePlayer();
+        }
+
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) searchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 disToWalkPoint = transform.position - walkPoint;
+
+        if (disToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void searchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void chasePlayer()
+    {
+        currentLoc = transform.position;
+        agent.SetDestination(player.position);
+        Navigation.isMoving = false;
+
+    }
+
+}
+
