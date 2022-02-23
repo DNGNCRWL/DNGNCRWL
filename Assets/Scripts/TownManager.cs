e480391c89e88c1d9c7f67966b7390517d837cca
James Yeazel <dwarfdwarf99@gmail.com>
2022-02-23 01:23:41 -0600

    Town changes
    Added Town scene, created buttons to open menus for town actions.

---
+++ Assets/Scripts/TownManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TownManager : MonoBehaviour
+{
+    public GameObject PartySwapMenu;
+    public GameObject RecruitMenu;
+    public GameObject RestMenu;
+    public GameObject StoreMenu;
+    public GameObject PartySwapButton;
+    public GameObject RecruitButton;
+    public GameObject RestButton;
+    public GameObject StoreButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void toggleActive(GameObject target) {
+        if (target.activeSelf) {
+            target.SetActive(false);
+        } else {
+            target.SetActive(true);
+        }
+    }
+
+    public void toggleBackground() {
+        toggleActive(PartySwapButton);
+        toggleActive(RecruitButton);
+        toggleActive(RestButton);
+        toggleActive(StoreButton);
+    }
+}
