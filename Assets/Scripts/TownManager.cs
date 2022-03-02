77ac248a8d598c07b704ba31ece237f1921f0e7c
James Yeazel <dwarfdwarf99@gmail.com>
2022-03-02 15:24:58 -0600

    Recruit character menu
    Worked on making recruitable character names appear

--- Assets/Scripts/TownManager.cs
+++ Assets/Scripts/TownManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class TownManager : MonoBehaviour
 {
@@ -10,10 +11,32 @@ public class TownManager : MonoBehaviour
     public GameObject RestButton;
     public GameObject StoreButton;
 
+    //Player Characters
+    public CharacterSheet Char1;
+    public CharacterSheet Char2;
+    public CharacterSheet Char3;
+    public CharacterSheet Char4;
+
+    //Recruitable Characters
+    public CharacterSheet RecChar1;
+    public CharacterSheet RecChar2;
+    public CharacterSheet RecChar3;
+    public CharacterSheet RecChar4;
+
+    public TextMeshProUGUI RecChar1Name;
+    public TextMeshProUGUI RecChar2Name;
+    public TextMeshProUGUI RecChar3Name;
+    public TextMeshProUGUI RecChar4Name;
+
+    void Awake() {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("RecChar1 Name: " + RecChar1.GetCharacterName());
+        setRecCharInfo();
     }
 
     // Update is called once per frame
@@ -21,6 +44,7 @@ public class TownManager : MonoBehaviour
     {
 
     }
+    
     public void toggleActive(GameObject target) {
         if (target.activeSelf) {
             target.SetActive(false);
@@ -34,5 +58,16 @@ public class TownManager : MonoBehaviour
         toggleActive(RecruitButton);
         toggleActive(RestButton);
         toggleActive(StoreButton);
+    }
+
+    public void rest() {
+        //Char1.RecoverDamage(10);
+    }
+
+    public void setRecCharInfo() {
+        RecChar1Name.text = RecChar1.GetCharacterName();
+        RecChar2Name.text = RecChar2.GetCharacterName();
+        RecChar3Name.text = RecChar3.GetCharacterName();
+        RecChar4Name.text = RecChar4.GetCharacterName();
     }
 }
