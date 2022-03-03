d84d9e446fe78b1ae7a6cff339f598be3c5dcaaf
James Yeazel <47924391+dwarfdwarf@users.noreply.github.com>
2022-03-03 16:52:48 -0600

    Update TownManager.cs

--- Assets/Scripts/TownManager.cs
+++ Assets/Scripts/TownManager.cs
@@ -169,7 +169,7 @@ public class TownManager : MonoBehaviour
     //Swap the char at the index from reserve characters to the player party
     public void addCharToParty(int index) {
         if (playerCharacters.Count < 4) {
-            CharacterSheet charSheet = reserveCharacters[index + (pageNumber + 1)];
+            CharacterSheet charSheet = reserveCharacters[index + (pageNumber * 2)];
             playerCharacters.Add(charSheet);
             GM.playerCharacters.Add(charSheet);
             reserveCharacters.Remove(charSheet);
