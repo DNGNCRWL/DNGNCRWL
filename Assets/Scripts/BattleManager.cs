cbfd8dfe7f174d25cd9fd58a33c49710f52a2f28
SOUNDWAVE\Raphael Azcueta <rrazcueta@gmail.com>
2022-03-05 17:06:38 -0600

    Setup for Dungeon Nav to Battle
    Setting it up so it's easy to move from Dungeon Nav to Battle

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -6,6 +6,8 @@ using DG.Tweening;
 public class BattleManager : MonoBehaviour
 {
     public static BattleManager BM;
+    static EnemyEncounter ENEMY_ENCOUNTER;
+    public static void SetENEMY_ENCOUNTER(EnemyEncounter enemyEncounter){ENEMY_ENCOUNTER = enemyEncounter;}
     MenuManager MM;
 
     [SerializeField] Party playerParty;
@@ -67,11 +69,15 @@ public class BattleManager : MonoBehaviour
     IEnumerator Start()
     {
         GameManager.GM.SetText("Enemies appeared");
+        if(ENEMY_ENCOUNTER != null)
+            GameManager.GM.AddText(ENEMY_ENCOUNTER.GetEncounterName());
         MM.CloseAllMenus();
         
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(3);
 
-        yield return StartCoroutine(Combat());
+        GameManager.GoToDungeonNavigation();
+
+        //yield return StartCoroutine(Combat());
     }
 
     //BATTLE STATES
